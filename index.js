add7a32b73b5353f976e3653a5cfbd2f05e9fccc
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-09 16:27:47 +0530

    API Working, Parameters to be added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function (req, res) {
-	res.send("Aur jhaantu");
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+	const url =
+		"https://sv443.net/jokeapi/v2/joke/Programming?format=txt&type=single";
+	// res.send("Successfull");
+	https.get(url, function (response) {
+		console.log(response.statusCode);
+		response.on("data", function (data) {
+			// const jokeData = JSON.parse(data);
+			res.write("<h1>Joke is </h1>" + data);
+			res.send();
+		});
+	});
 });
 
 app.listen(3000, function () {
