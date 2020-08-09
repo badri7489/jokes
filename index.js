bcf6484932c97859b1e9ef4cb5909fd0f61d3f76
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-09 20:11:14 +0530

    PORT changed for heroku

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.post("/", function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Jaa... jaa kr port 3000 pe dekh insaan aaya hai ki bhagwan");
 });
