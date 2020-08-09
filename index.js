09da579549936e7b1852a1ee365b911684b4f37f
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-09 20:10:02 +0530

    UI improved

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const bodyParser = require("body-parser");
 const https = require("https");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
+	const category = req.body.category;
+	const type = req.body.type;
+	const blacklistFlags = req.body.blacklistFlags;
 	const url =
-		"https://sv443.net/jokeapi/v2/joke/Programming?format=txt&type=single";
-	// res.send("Successfull");
+		"https://sv443.net/jokeapi/v2/joke/" +
+		category +
+		"?type=" +
+		type +
+		"&blacklistFlags=" +
+		blacklistFlags +
+		"&format=txt";
 	https.get(url, function (response) {
 		console.log(response.statusCode);
 		response.on("data", function (data) {
-			// const jokeData = JSON.parse(data);
+			console.log(url);
 			res.write("<h1>Joke is </h1>" + data);
 			res.send();
 		});
